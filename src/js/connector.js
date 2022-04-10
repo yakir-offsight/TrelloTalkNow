144fbb98ba702eef2dc1c1ac92cf80f3ff89f808
yakir <yakir22@gmail.com>
2022-04-10 16:29:08 +0300

    nu

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,6 +1,4 @@
-var WHITE_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg';
-var BLACK_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-black.svg';
-var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
+var ICON = 'https://trello-members.s3.amazonaws.com/62527cb59e2a8f234011782e/b87c0a65ee0b19b00320a8d7fd271e49/50.png';
 
 var onBtnClick = function (t, opts) {
 	console.log('Someone clicked the button2');
@@ -14,23 +12,19 @@ window.TrelloPowerUp.initialize({
 	'board-buttons': function (t, opts) {
 		return [{
 			// we can either provide a button that has a callback function
-			icon: {
-				dark: 'https://trello-members.s3.amazonaws.com/62527cb59e2a8f234011782e/b87c0a65ee0b19b00320a8d7fd271e49/50.png',
-				light: 'https://trello-members.s3.amazonaws.com/62527cb59e2a8f234011782e/b87c0a65ee0b19b00320a8d7fd271e49/50.png'
-			},
+			icon: ICON,
 			text: 'Talk Now',
 			callback: onBtnClick,
 			condition: 'edit'
 		}]
 	},
-	'card-buttons': function (t, opts) {
+	'card-badges': function (t, opts) {
 		return [{
 			// usually you will provide a callback function to be run on button click
 			// we recommend that you use a popup on click generally
-			icon: GRAY_ICON, // don't use a colored icon here
+			icon: ICON, // don't use a colored icon here
 			text: 'Open Popup',
-			callback: onBtnClick,
-			condition: 'edit'
+			color: 'green'
 		}]
 	}
 
