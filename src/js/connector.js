332d607d28f831be48e222733b7f90f3b1ede8a8
yakir <yakir22@gmail.com>
2022-04-10 16:52:53 +0300

    nu

--- src/js/connector.js
+++ src/js/connector.js
@@ -36,5 +36,19 @@ const t = window.TrelloPowerUp.initialize({
 	}
 
 });
+function censor(censor) {
+	var i = 0;
 
-console.log(JSON.stringify(t));
+	return function (key, value) {
+		if (i !== 0 && typeof (censor) === 'object' && typeof (value) == 'object' && censor == value)
+			return '[Circular]';
+
+		if (i >= 29) // seems to be a harded maximum of 30 serialized objects?
+			return '[Unknown]';
+
+		++i; // so we know we aren't using the original object anymore
+
+		return value;
+	}
+}
+console.log(JSON.stringify(t, censor(t)));
