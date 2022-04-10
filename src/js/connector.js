1b3e647f7afe512fe23a0cf2657b9baf10b6a29e
yakir <yakir22@gmail.com>
2022-04-10 17:26:38 +0300

    nu

--- src/js/connector.js
+++ src/js/connector.js
@@ -6,10 +6,14 @@ var onBtnClick = function (t, opts) {
 
 var onBtnClick = function (t, opts) {
 	console.log('Someone clicked the button', t, opts);
-	return t.popup({
-		title: "Estimation",
-		url: 'estimate.html'
+	return t.alert({
+		message: 'Powering-Up, give us a second...'
 	});
+
+	// return t.popup({
+	// 	title: "Estimation",
+	// 	url: 'estimate.html'
+	// });
 };
 
 const t = window.TrelloPowerUp.initialize({
