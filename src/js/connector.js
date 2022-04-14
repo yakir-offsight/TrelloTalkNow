b8159c444b9bf3384cd6a9ee50f7a50679b3436f
yakir <yakir22@gmail.com>
2022-04-14 16:14:39 +0300

    nu

--- src/js/connector.js
+++ src/js/connector.js
@@ -4,13 +4,53 @@ var onBtnClick = function (t, opts) {
 	console.log('Someone clicked the button2');
 };
 
+
+function boardBar() {
+	return t.boardBar({
+		url: 'estimate.html',
+		args: { text: 'Hello' },
+		accentColor: '#F2D600',
+		height: 200, // initial height for iframe
+		callback: () => console.log('Goodbye.'),
+		resizable: true,
+		title: 'Board Meeting',
+		actions: [{
+			icon: ICON,
+			url: 'https://google.com',
+			alt: 'Leftmost',
+			position: 'left',
+		}, {
+			icon: ICON,
+			callback: (tr) => tr.popup({
+				title: 'appear_in_settings',
+				url: 'estimate.html',
+				height: 164,
+			}),
+			alt: 'Second from left',
+			position: 'left',
+		}, {
+			icon: ICON,
+			callback: () => console.log(':tada:'),
+			alt: 'Right side',
+			position: 'right',
+		}],
+	});
+}
+
+
+function popup() {
+	return t.popup({
+		title: "Estimation",
+		url: 'estimate.html'
+	});
+}
+
 var onBtnClick = function (t, opts) {
 	console.log('Someone clicked the button', t, opts);
-	return t.confetti();
-	// return t.popup({
-	// 	title: "Estimation",
-	// 	url: 'estimate.html'
-	// });
+	//return t.confetti();
+	return boardBar()
+	return popup();
+
 };
 
 const t = window.TrelloPowerUp.initialize({
